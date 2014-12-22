48ccea14fe760ade1b3ca7a08e12ed00047477c0
Nicolas DURAN <duran_n@epita.fr>
2014-12-22 20:23:45 +0100

    Update test suite

--- test.c
+++ test.c
@@ -4,26 +4,39 @@
 #include "utils.h"
 #include "diam.h"
 #include "prelim.h"
+#include "test.h"
 
+#define NB_TRY 5
 
 void print_header()
 {
 	printf("********************************************\n");
 	printf("***           VLG Test Suite             ***\n");
 	printf("*** By Nicolas DURAN & Rodolphe Guitteny ***\n");
-	printf("********************************************\n");
+	printf("********************************************\n\n\n");
 }
 
 void test_suite(graph* g)
 {
+	print_header();
 	srand(time(NULL));
-	test_fringe(g);
+	
+	printf("********************************************\n");
+	printf("***  FRINGE Algorithm - Unordered Graph  ***\n");	
+	printf("********************************************\n\n");
+	
+	int nb_try = NB_TRY;
+	while (nb_try--)	
+		test_fringe(g);
 }
 
 
 void test_fringe(graph* g)
 {
+	printf("Generating random start & end\n");
 	int start = rand()%g->n;
 	int end = rand()%g->n;
+	printf("Search path from %d to %d\n", start, end);
 	fringe(g, start, end);
+	printf("------\n");
 }
--- test.h
+++ test.h
@@ -1,6 +1,9 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include "prelim.h"
+#include "diam.h"
+
 void test_fringe(graph* g);
 void print_header();
 void test_suite(graph* g);
